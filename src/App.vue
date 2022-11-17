ce3b00b03fba453ed0e90525d741cc14894cbb49
Wesley Parr <rustchain64@gmail.com>
2022-11-17 12:29:50 -0700

    pie logo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/pie_logo.png" width="300">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
