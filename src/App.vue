a675cc34a16f93638a9ab15c878f12286a021ce8
Wesley Parr <rustchain64@gmail.com>
2022-11-18 00:09:03 -0700

    test .envs

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,53 @@
+
+
 <template>
-  <img alt="Vue logo" src="./assets/pie_logo.png" width="300">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container pt-4 pb-4">
+    <img alt="Vue logo" src="./assets/pie_logo.png" width="250">
+    <div class="my-row">
+      <div class="value">ROOT API ::  {{ this.envValue }}</div>
+      <button @click="showRootApi()">SHOW ROOT API</button>
+    </div>
+
+    <div class="my-row">
+      <div class="value"> QUOTE API ::  {{ this.qodApiUrl }}</div>
+      <button @click="showQuoteApi()">SHOW QUOTE API</button>
+    </div>
+    <!-- <HelloWorld msg="Apple Pie"/> -->
+  </div>
+  <div class="container pt-4 pb-4">
+      <!-- <router-view /> -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {     
+      envValue: 'test',
+      qodApiUrl: 'greetings',
+    };
+  },
+  methods: {
+    showRootApi() {
+      console.log(process.env.VUE_APP_ROOT_API);
+      this.envValue = process.env.VUE_APP_ROOT_API;
+      //router.push(this.returnUrl || '/adminDashboard');
+    },
+    showQuoteApi() {
+      console.log(process.env.VUE_APP_QOD_API_URL);
+      this.qodApiUrl = process.env.VUE_APP_QOD_API_URL;
+      //router.push(this.returnUrl || '/adminDashboard');
+    },
+    mounted() {
+      console.log("mounted");      
+    },
   }
 }
 </script>
 
 <style>
+@import '@/assets/main.css';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +55,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.my-row {
+  width: 50vw;
+  display: flex;
+  justify-content: center;
+}
+.value {
+  margin-right: 10%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+//import { router } from './router';
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const app = createApp(App);
+
+app.use(createPinia());
+//app.use(router);
+
+app.mount('#app');
