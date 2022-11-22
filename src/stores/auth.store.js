b262f71db99d1fb984957e790cf986eb1a075bc3
Wesley Parr <rustchain64@gmail.com>
2022-11-22 12:25:34 -0700

    clean up

--- src/stores/auth.store.js
+++ src/stores/auth.store.js
@@ -38,7 +38,6 @@ export const useAuthStore = defineStore({
         localStorage.setItem("user", JSON.stringify(user));
 
         // redirect to previous url or default to home page
-        //router.push(this.returnUrl || '/');
         router.push(this.returnUrl || "/");
       } catch (error) {
         const alertStore = useAlertStore();
